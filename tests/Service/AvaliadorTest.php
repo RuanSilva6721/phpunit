a7231f08db66c01069ee52203955a0335ab3cb8e
Ruan Silva <ruansilva6721@gmail.com>
2023-04-17 11:08:02 -0300

    add test of array

--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -9,7 +9,7 @@ use PHPUnit\Framework\TestCase;
 
 class AvaliadorTest extends TestCase {
 
-    public function test_Avaliador_pegar_ultimo_ordem_crescente(){
+    public function test_Avaliador_pegar_maior_ordem_crescente(){
 
         $leilão = new Leilao('Celta 147 0KM');
 
@@ -28,7 +28,7 @@ class AvaliadorTest extends TestCase {
 
         self::assertEquals(2500, $maiorValor);
     }
-    public function test_Avaliador_pegar_ultimo_ordem_descrescente(){
+    public function test_Avaliador_pegar_maior_ordem_descrescente(){
 
         $leilão = new Leilao('Celta 147 0KM');
 
@@ -46,6 +46,68 @@ class AvaliadorTest extends TestCase {
         echo $maiorValor;
 
         self::assertEquals(2500, $maiorValor);
+    }
+    public function test_Avaliador_pegar_menor_ordem_descrescente(){
+
+        $leilão = new Leilao('Celta 147 0KM');
+
+        $ruan = new Usuario('Ruan');
+        $carlos = new Usuario('Carlos');
+
+        $leilão->recebeLance( new Lance($ruan, 2500));
+        $leilão->recebeLance( new Lance($carlos, 2000));
+
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilão);
+
+        $menorValor = $leiloeiro->getMenorValor();
+
+        echo $menorValor;
+
+        self::assertEquals(2000, $menorValor);
+    }
+    public function test_Avaliador_pegar_menor_ordem_crescente(){
+
+        $leilão = new Leilao('Celta 147 0KM');
+
+        $ruan = new Usuario('Ruan');
+        $carlos = new Usuario('Carlos');
+
+        $leilão->recebeLance( new Lance($ruan, 2000));
+        $leilão->recebeLance( new Lance($carlos, 2500));
+
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilão);
+
+        $menorValor = $leiloeiro->getMenorValor();
+
+        echo $menorValor;
+
+        self::assertEquals(2000, $menorValor);
+    }
+
+    public function test_avaliador_buscar_3_maiores_valores(){
+        $leilao = new Leilao('Fiat 147 0KM');
+        $ruan = new Usuario('Ruan');
+        $maria = new Usuario('Maria');
+        $ana = new Usuario('Ana');
+        $jorge = new Usuario('Jorge');
+
+        $leilao->recebeLance(new Lance($ana, 1500));
+        $leilao->recebeLance(new Lance($maria, 2000));
+        $leilao->recebeLance(new Lance($ruan, 2500));
+        $leilao->recebeLance(new Lance($jorge, 2300));
+
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilao);
+
+        $maioresValores = $leiloeiro->getMaioresLances();
+
+        static::assertCount(3, $maioresValores);
+        static::assertEquals(2500, $maioresValores[0]->getValor());
+        static::assertEquals(2300, $maioresValores[1]->getValor());
+        static::assertEquals(2000, $maioresValores[2]->getValor());
+
     }
     
 }
